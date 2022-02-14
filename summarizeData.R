64b16a8e86214e369353b555347827110587aa09
Michelle Pan <pan@ip-172-31-89-183.ec2.internal>
2022-02-14 22:51:10 +0000

    2

--- summarizeData.R
+++ summarizeData.R
@@ -1,5 +1,5 @@
-source('/home/pan/ECON 470/HCRIS/data-code/_HCRIS_Data.R')
-
+library(tidyverse)
+library(data.table)
 HCRIS_Data_v1996 <- readRDS("~/ECON 470/HCRIS/Data/HCRIS_Data_v1996.rds")
 HCRIS_Data_v2010 <- readRDS("~/ECON 470/HCRIS/Data/HCRIS_Data_v2010.rds")
 
@@ -11,3 +11,10 @@ summary(HCRIS_Data_v2010)
 
 #1
 n_distinct(HCRIS_Data_v1996$provider_number)
+
+#2
+n_occur <- data.frame(table(HCRIS_Data_v1996$provider_number))
+# table of provider number and how many times they occur
+n_occur[n_occur$Freq > 1,]
+repeats <- HCRIS_Data_v1996[HCRIS_Data_v1996$provider_number %in% n_occur$Var1[n_occur$Freq > 1],]
+
